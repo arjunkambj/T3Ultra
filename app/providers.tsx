c8229c4a41986a2477ed49a42ddababd0528a1f8
Arjun Kamboj <arjunkamboj87699@gmail.com>
2025-06-09 23:25:34 +0530

    final markdown

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { ThemeProviderProps } from "next-themes";
-
+import { Provider as JotaiProvider } from "jotai";
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { useRouter } from "next/navigation";
@@ -25,7 +25,9 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <HeroUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      <NextThemesProvider {...themeProps}>
+        <JotaiProvider>{children}</JotaiProvider>
+      </NextThemesProvider>
     </HeroUIProvider>
   );
 }
