cd395a8ce4cdba73228e1b13185244a7a51c7a48
Arjun Kamboj <arjunkamboj87699@gmail.com>
2025-06-17 10:49:02 +0530

    building

--- app/(app)/(chat)/agent/[agentId]/page.tsx
+++ app/(app)/(chat)/agent/[agentId]/page.tsx
@@ -1,11 +1,3 @@
-import AgentDetail from "@/components/agent/AgentDetail";
-
-interface AgentPageProps {
-  params: {
-    agentId: string;
-  };
-}
-
-export default function AgentPage({ params }: AgentPageProps) {
-  return <AgentDetail agentId={params.agentId} />;
+export default function AgentPage() {
+  return <div>AgentPage</div>;
 }
