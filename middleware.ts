0a05b9970edeacb96507dd463cd08c5f95cc0123
Arjun Kamboj <arjunkamboj87699@gmail.com>
2025-07-03 19:55:33 +0530

    middlewear cookie update

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
 
-export default convexAuthNextjsMiddleware(
-  (request, { convexAuth }) => {
-    // ...
-  },
-  { cookieConfig: { maxAge: 60 * 60 * 24 * 30 } },
-);
+export default convexAuthNextjsMiddleware((request, { convexAuth }) => {}, {
+  cookieConfig: { maxAge: 60 * 60 * 24 * 30 },
+});
+
 export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
